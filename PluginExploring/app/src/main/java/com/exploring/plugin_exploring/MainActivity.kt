1d9b45aa06dc9725ded53aa08cad9b551cce65b7
dengshiwei <dengshiwei>
2022-10-20 10:29:07 +0800

    add plugin demo

---
+++ PluginExploring/app/src/main/java/com/exploring/plugin_exploring/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.exploring.plugin_exploring
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
